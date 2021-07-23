4cac0ba5f2669613a165f5fc746a6f18740b4b46
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-07-23 13:54:52 -0500

    Función para calcular altura de un triángulo isósceles

--- figuras.js
+++ figuras.js
@@ -8,12 +8,50 @@ const areaCuadrado = (lado) => lado * lado;
 console.groupEnd()
 
 // Código del triángulo
+console.group("Triángulos");
 
 const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
 
 const areaTriangulo = (base, altura) => (base * altura) / 2;
     
 console.groupEnd()
+
+//Altura triángulo isósceles
+
+const alturaTrianguloIsosceles = function (a, b, c) {
+    let A;
+    if(a > b && b === c && a >= 2*b || b > a && a === c && b >= 2*a || c > a && a === b && c >= 2*a) {
+        alert('Valores no congruentes');
+    } else if(a > b && a === c || a < b && a === c) {
+        A = Math.sqrt(Math.pow(a,2) - Math.pow(b/2, 2));
+        return A;
+    } else if(a > c && a === b || a < c && a === b) {
+        A = Math.sqrt(Math.pow(a,2) - Math.pow(c/2, 2));
+        return A;
+    } else if(b > a && b === c || b < a && b === c) {
+        A = Math.sqrt(Math.pow(b,2) - Math.pow(a/2, 2));
+        return A;
+    } else {
+        alert('El triangulo no es Isósceles');
+    }
+}  
+
+function alturaTrianguloIsoscelesPlatzi(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
     
 // Código del círculo
 console.group("Círculos");
@@ -37,7 +75,7 @@ function calcularPerimetroCuadrado(){
 };
 
 function calcularAreaCuadrado() {
-    const input = document.getElementById("InputCuadrado1");
+    const input = document.getElementById("InputCuadrado");
     const value = input.value;
     const area = areaCuadrado(value);
     alert (area);
