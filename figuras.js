2f88aaddf7ee4ff1c9217a4b5821e7dbd77addba
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-07-22 23:52:51 -0500

    Se crean los métodos para calcular las areas y perimetros de las figuras

--- figuras.js
+++ figuras.js
@@ -28,3 +28,53 @@ const perimetroCirculo = (radio) => diametroCirculo(radio) * Math.PI;
 const areaCirculo = (radio) => (radio * radio) * Math.PI;
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert (perimetro);
+};
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado1");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert (area);
+};
+
+
+function calcularPerimetroTriangulo(){
+    const l1 = document.getElementById("InputTriangulo1");
+    const lado1 = parseFloat(l1.value);
+    const l2 = document.getElementById("InputTriangulo2");
+    const lado2 = parseFloat(l2.value);
+    const lb = document.getElementById("InputTriangulob");
+    const ladob = parseFloat(lb.value);
+    const perimetro = perimetroTriangulo(lado1, lado2, ladob);
+    alert (perimetro);
+};
+
+function calcularAreaTriangulo() {
+    const lb = document.getElementById("InputTriangulob");
+    const ladob = parseFloat(lb.value);
+    const lh = document.getElementById("InputTrianguloh");
+    const ladoh = parseFloat(lh.value);
+    const area = areaTriangulo(ladob, ladoh);
+    alert (area);
+};
+
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const perimetro = perimetroCirculo(value);
+    alert (perimetro);
+};
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    const area = areaCirculo(value);
+    alert (area);
+};
