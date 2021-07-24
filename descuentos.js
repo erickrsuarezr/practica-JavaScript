d0a86e85b1d309993384df9090fe243c90f8370c
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-07-23 19:20:52 -0500

    Creación de función para imprimir el resultado mediante el botón

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,15 @@ function calcularPrecioConDescuento(precio, descuento) {
     const PrecioConDescuento = (precio * porcentajePrecioConDescuento) /100;
     return PrecioConDescuento;
 }  
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const price = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discount = inputDiscount.value;
+
+    const PrecioConDescuento = calcularPrecioConDescuento(price,discount);
+
+    const resultP = document.getElementById("ResultPrice");
+    resultP.innerText= "El precio con descuento es: $" +  PrecioConDescuento;
+}
