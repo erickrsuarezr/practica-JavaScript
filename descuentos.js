06ecc4299627618a3ba05b5ecbe8f20c74c11b77
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-07-23 20:40:30 -0500

    Método para cupones, no funcional, pero idea

--- descuentos.js
+++ descuentos.js
@@ -15,3 +15,34 @@ function onClickButtonPriceDiscount() {
     const resultP = document.getElementById("ResultPrice");
     resultP.innerText= "El precio con descuento es: $" +  PrecioConDescuento;
 }
+
+// const coupons = [
+//     {
+//         name: "Erick_es_Batman",
+//         discount: 15,
+//     },
+//     {
+//         name: "pero_no_le_digas_a_nadie",
+//         discount: 30,
+//     },
+//     {
+//         name: "es_un_secreto",
+//         discount: 25,
+//     }
+// ];
+
+// const isCouponValueValid = function (coupon) {
+//     return coupon.name === couponValue;
+// };
+
+// const userCoupon = coupons.find(isCouponValueValid);
+
+// if (!userCoupon) {
+//     alert("El cupón " + userCouponValue + "no es válido");
+// }else {
+//     const descuento = userCoupon.discount;
+//     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+//     const resultP = document.getElementById("ResultP");
+//     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+// }
